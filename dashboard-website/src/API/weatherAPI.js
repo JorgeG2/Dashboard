bb244be45614256af4e525604e655a86857a7872
JorgeG2 <Jorgegonzales191421@Gmail.com>
2024-01-10 19:45:46 -0500

    deleted api key for safety

--- dashboard-website/src/API/weatherAPI.js
+++ dashboard-website/src/API/weatherAPI.js
@@ -2,8 +2,7 @@ const fetch = require("node-fetch");
 
 async function getWeatherData() {
   try {
-    const url =
-      "https://api.openweathermap.org/data/2.5/weather?lat=39.46381&lon=-74.51205&appid=0efff71c6e567b425f7def73acf43a25";
+    const url = "";
     const response = await fetch(url);
 
     if (!response.ok) {
